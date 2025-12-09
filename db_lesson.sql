7ea81c389c82ce0b8c74007656ff6a9fdfa4676c
masaya tsukada <mmdunity9856@gmail.com>
2025-12-09 10:51:09 +0900

    練習問題Q6を修正

--- db_lesson.sql
+++ db_lesson.sql
@@ -1,10 +1,10 @@
 /**Q1**/
 
 CREATE TABLE`departments`(
-`department_id` INT unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-`name` VARCHAR(20) NOT NULL,
-`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-`updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `department_id` INT unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(20) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 
@@ -28,31 +28,31 @@ VALUES
 /**追加する人の条件(people)**/
 INSERT INTO `people` (`name`, `department_id`)
 VALUES
-('営業1', 1),
-('営業2', 1),
-('営業3', 1),
-('開発1', 2),
-('開発2', 2),
-('開発3', 2),
-('開発4', 2),
-('経理1', 3),
-('人事1', 4),
-('情報1', 5);
+  ('営業1', 1),
+  ('営業2', 1),
+  ('営業3', 1),
+  ('開発1', 2),
+  ('開発2', 2),
+  ('開発3', 2),
+  ('開発4', 2),
+  ('経理1', 3),
+  ('人事1', 4),
+  ('情報1', 5);
 
 
 /**追加する日報の条件(reports)**/
 INSERT INTO `reports` (`person_id`, `content`)
 VALUES
-('1', '営業1の日報です。'),
-('1', '営業2の日報です。'),
-('1', '営業3の日報です。'),
-('2', '開発1の日報です。'),
-('2', '開発2の日報です。'),
-('2', '開発3の日報です。'),
-('2', '開発4の日報です。'),
-('3', '経理1の日報です。'),
-('4', '人事1の日報です。'),
-('5', '情報1の日報です。');
+  ('1', '営業1の日報です。'),
+  ('1', '営業2の日報です。'),
+  ('1', '営業3の日報です。'),
+  ('2', '開発1の日報です。'),
+  ('2', '開発2の日報です。'),
+  ('2', '開発3の日報です。'),
+  ('2', '開発4の日報です。'),
+  ('3', '経理1の日報です。'),
+  ('4', '人事1の日報です。'),
+  ('5', '情報1の日報です。');
 
 
 /**Q4**/
@@ -77,10 +77,10 @@ WHERE
 
 /**Q6**/
 SELECT
-  どのテーブルから、どのカラムを取得するか指定すること。
+  `people`テーブルから、`name`, `email`, `age` カラムを取得するか指定すること。
 FROM
-  どのテーブルか指定すること。
+  `people` テーブルを指定すること。
 WHERE
-  条件に合うカラムを取得すること。
+  `department_id` = 1 という条件に合うカラムを取得すること。
 ORDER BY
-  レコードの順番を並び替えてカラムを取得すること。
+   レコードの作成日時順に並び替えてカラムを取得すること。
